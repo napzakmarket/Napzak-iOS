68495f32e1dbc2ec8495fad2c8fc29bf76a802f3
JoHyerin <gpfls468@gmail.com>
2025-03-14 06:33:15 +0900

    [Add] #1 - Presentation 하위 폴더 생성 및 .gitkeep 파일 추가

---
+++ Napzak-iOS/Napzak-iOS/Presentation/TabBar/View/ExtraView.swift
@@ -0,0 +1,18 @@
+//
+//  ExtraView.swift
+//  Napzakmarket-iOS
+//
+//  Created by 어진 on 1/7/25.
+//
+// 뷰 연결 후에 삭제 예정
+
+import SwiftUI
+
+struct ExtraView: View {
+    var body: some View {
+        NavigationView {
+            Text("임시뷰")
+        }
+    }
+}
+
