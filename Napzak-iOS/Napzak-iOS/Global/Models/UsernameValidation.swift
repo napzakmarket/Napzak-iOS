44aa78c9368c535c58c362e87b56d9d7ba9fd07c
joho2022 <josama1997@naver.com>
2025-04-02 02:25:15 +0900

    [Add] #6 - 유저네임 검증 상태 관리 enum 구현

---
+++ Napzak-iOS/Napzak-iOS/Global/Models/UsernameValidation.swift
@@ -0,0 +1,45 @@
+//
+//  UsernameValidation.swift
+//  Napzak-iOS
+//
+//  Created by 조호근 on 4/1/25.
+//
+
+import SwiftUI
+
+enum UsernameValidation {
+    case valid
+    case empty
+    case invalidSapce
+    case invalidSpecialChar
+    case invalidProfanity
+    case invalidDuplicate
+    
+    var message: String {
+        switch self {
+        case .valid:
+            return "사용할 수 있는 이름이에요!"
+        case .invalidSapce:
+            return "띄어쓰기를 포함할 수 없어요."
+        case .invalidSpecialChar:
+            return "특수기호를 사용할 수 없어요."
+        case .invalidProfanity:
+            return "욕설이나 비속어를 사용할 수 없어요."
+        case .invalidDuplicate:
+            return "이미 사용중인 이름이에요"
+        case .empty:
+            return ""
+        }
+    }
+    
+    var color: Color {
+        switch self {
+        case .valid:
+            return Color.napzakState(.green)
+        case .invalidSapce, .invalidSpecialChar, .invalidProfanity, .invalidDuplicate:
+            return Color.napzakState(.red)
+        case .empty:
+            return .clear
+        }
+    }
+}
