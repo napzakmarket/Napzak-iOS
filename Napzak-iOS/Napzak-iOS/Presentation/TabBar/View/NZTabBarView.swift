c9a632c5e50e429fabc743ad6798ef6a3510422a
JoHyerin <gpfls468@gmail.com>
2025-04-06 23:44:42 +0900

    [Feat] #7 - NZTabBarView 구현

---
+++ Napzak-iOS/Napzak-iOS/Presentation/TabBar/View/NZTabBarView.swift
@@ -0,0 +1,255 @@
+//
+//  NZTabBarView.swift
+//  Napzak-iOS
+//
+//  Created by 조혜린 on 4/2/25.
+//
+
+import SwiftUI
+
+struct NZTabBarView: View {
+    
+    //MARK: - Enum
+    
+    enum Tab {
+        case home
+        case search
+        case chat
+        case my
+    }
+    
+    //MARK: - Property Wrappers
+    
+    @State private var selectedTab: Tab = .home
+    @State private var previousTab: Tab = .home
+    @State private var isRegisterTabSelected = false
+    
+    @State var path = NavigationPath()
+    
+    //MARK: - Body
+        
+    var body: some View {
+        NavigationStack(path: $path) {
+            ZStack(alignment: .bottom) {
+                TabView(selection: $selectedTab) {
+                    Group {
+                        HView(path: $path)
+                            .tag(Tab.home)
+                        SView(path: $path)
+                            .tag(Tab.search)
+                        CView(path: $path)
+                            .tag(Tab.chat)
+                        MView()
+                            .tag(Tab.my)
+                    }
+                    .toolbar(.hidden, for: .tabBar)
+                }
+                
+                if  isRegisterTabSelected {
+                    Color.clear
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            isRegisterTabSelected = false
+                        }
+                }
+                
+                VStack(spacing: 10) {
+                    if  isRegisterTabSelected {
+                        RegisterFloatingView()
+                    }
+                    tabBar
+                }
+            }
+            .edgesIgnoringSafeArea(.bottom)
+            .navigationDestination(for: String.self) { string in
+                if string == "SView" {
+                    SView(path: $path)
+                } else if string == "RView" {
+                    RView(path: $path)
+                }
+            }
+        }
+    }
+    
+    var tabBar: some View {
+        HStack {
+            Button {
+                selectedTab = .home
+                isRegisterTabSelected = false
+            } label: {
+                VStack(alignment: .center, spacing: 5) {
+                    Image(isSelectedTab(.home) ? .iconTabHomeSelected : .iconTabHomeDefault)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 24)
+                    Text("홈")
+                        .applyNapzakFont(.caption1SemiBold12)
+                        .foregroundStyle(isSelectedTab(.home) ? Color.napzakPrimary(.purple500) : Color.napzakGrayScale(.gray200))
+                }
+            }
+            Spacer()
+            Button {
+                selectedTab = .search
+                isRegisterTabSelected = false
+            } label: {
+                VStack(alignment: .center, spacing: 5) {
+                    Image(isSelectedTab(.search) ? .iconTabSearchSelected : .iconTabSearchDefault)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 24)
+                    Text("탐색")
+                        .applyNapzakFont(.caption1SemiBold12)
+                        .foregroundStyle(isSelectedTab(.search) ? Color.napzakPrimary(.purple500) : Color.napzakGrayScale(.gray200))
+                }
+            }
+            Spacer()
+            Button {
+                isRegisterTabSelected.toggle()
+            } label: {
+                VStack(alignment: .center, spacing: 5) {
+                    Image(isRegisterTabSelected ? .iconTabRegisterSelected : .iconTabRegisterDefault)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 24)
+                    Text("등록")
+                        .applyNapzakFont(.caption1SemiBold12)
+                        .foregroundStyle(isRegisterTabSelected ? Color.napzakPrimary(.purple500) : Color.napzakGrayScale(.gray200))
+                }
+            }
+            Spacer()
+            Button {
+                selectedTab = .chat
+                isRegisterTabSelected = false
+            } label: {
+                VStack(alignment: .center, spacing: 5) {
+                    Image(isSelectedTab(.chat) ? .iconTabChatSelected : .iconTabChatDefault)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 24)
+                    Text("채팅")
+                        .applyNapzakFont(.caption1SemiBold12)
+                        .foregroundStyle(isSelectedTab(.chat) ? Color.napzakPrimary(.purple500) : Color.napzakGrayScale(.gray200))
+                }
+            }
+            Spacer()
+            Button {
+                selectedTab = .my
+                isRegisterTabSelected = false
+            } label: {
+                VStack(alignment: .center, spacing: 5) {
+                    Image(isSelectedTab(.my) ? .iconTabMySelected : .iconTabMyDefault)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 24)
+                    Text("마이")
+                        .applyNapzakFont(.caption1SemiBold12)
+                        .foregroundStyle(isSelectedTab(.my) ? Color.napzakPrimary(.purple500) : Color.napzakGrayScale(.gray200))
+                }
+            }
+        }
+        .padding(.horizontal, 40)
+        .padding(.top, 14.5)
+        .padding(.bottom, 29.5)
+        .frame(height: 88)
+        .background(
+            Color.napzakGrayScale(.white)
+                .shadow(color: .black.opacity(0.4), radius: 0.4)
+        )
+    }
+}
+
+private extension NZTabBarView {
+    
+    //MARK: - Private Method
+    
+    func isSelectedTab(_ tab: Tab) -> Bool {
+        return selectedTab == tab && !isRegisterTabSelected
+    }
+}
+
+// 임시로 띄울 뷰, 삭제 예정
+struct HView: View {
+    
+    @Binding var path: NavigationPath
+    
+    var body: some View {
+        VStack {
+            Button {
+                print("")
+                path.append("SView")
+            } label: {
+                Text("눌러")
+                    .background(.red)
+            }
+            Text("홈")
+                .applyNapzakFont(.title1Bold22)
+        }
+    }
+}
+
+struct SView: View {
+    @Binding var path: NavigationPath
+    
+    var body: some View {
+        VStack {
+            Button {
+                print("버튼 눌림")
+                path.append("RView")
+            } label: {
+                Text("다음")
+                    .background(.red)
+            }
+            Text("탐색")
+                .applyNapzakFont(.title1Bold22)
+        }
+    }
+}
+
+struct RView: View {
+    
+    @Binding var path: NavigationPath
+    
+    var body: some View {
+        VStack {
+            Button {
+                print("버튼 눌림")
+                path = NavigationPath()
+            } label: {
+                Text("맨처음")
+                    .background(.red)
+            }
+            Text("등록")
+                .applyNapzakFont(.title1Bold22)
+        }
+    }
+}
+
+struct CView: View {
+    
+    @Binding var path: NavigationPath
+    
+    var body: some View {
+        VStack {
+            Button {
+                path.append("SView")
+            } label: {
+                Text("눌러")
+                    .background(.red)
+            }
+            Text("채팅")
+                .applyNapzakFont(.title1Bold22)
+        }
+    }
+}
+
+struct MView: View {
+    var body: some View {
+        Text("마이")
+            .applyNapzakFont(.title1Bold22)
+    }
+}
+
+#Preview {
+    NZTabBarView()
+}
