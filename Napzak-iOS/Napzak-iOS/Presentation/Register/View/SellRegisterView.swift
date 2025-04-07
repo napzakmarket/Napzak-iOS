f0427d5ac4d617eef8358fba7a72181b6d21c52f
OneTen19 <khy000426@naver.com>
2025-04-08 01:50:44 +0900

    [Feat] #8 - SellRegisterHeader UI 구현

---
+++ Napzak-iOS/Napzak-iOS/Presentation/Register/View/SellRegisterView.swift
@@ -0,0 +1,24 @@
+//
+//  SellRegisterView.swift
+//  Napzak-iOS
+//
+//  Created by OneTen on 4/8/25.
+//
+
+import SwiftUI
+
+struct SellRegisterView: View {
+    var body: some View {
+        VStack{
+            SellRegisterHeader()
+            ScrollView {
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            }
+        }
+
+    }
+}
+
+#Preview {
+    SellRegisterView()
+}
