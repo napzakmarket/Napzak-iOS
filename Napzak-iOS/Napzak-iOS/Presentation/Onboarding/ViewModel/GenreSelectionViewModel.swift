b0bdcdd69d174484f7592d01a27702439bde7dcd
joho2022 <josama1997@naver.com>
2025-04-08 02:23:18 +0900

    [Refactor] #11 - Task.sleep() 사용법 최신화

--- Napzak-iOS/Napzak-iOS/Presentation/Onboarding/ViewModel/GenreSelectionViewModel.swift
+++ Napzak-iOS/Napzak-iOS/Presentation/Onboarding/ViewModel/GenreSelectionViewModel.swift
@@ -28,7 +28,7 @@ class GenreSelectionViewModel: ObservableObject {
                 showToast = true
                 
                 Task {
-                    try? await Task.sleep(nanoseconds: 2_000_000_000)
+                    try? await Task.sleep(for: .seconds(2))
                     await MainActor.run {
                         self.showToast = false
                     }
