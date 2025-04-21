4bf1abf91f28006ee2dba6eb65468e49c6bf0066
JoHyerin <gpfls468@gmail.com>
2025-04-21 16:55:57 +0900

    [Feat] #24 - 장르 선택 모달 UI 구현

---
+++ Napzak-iOS/Napzak-iOS/Global/Components/Modal/View/GenreSelectModalView.swift
@@ -0,0 +1,154 @@
+//
+//  GenreSelectModalView.swift
+//  Napzak-iOS
+//
+//  Created by 조혜린 on 4/21/25.
+//
+
+import SwiftUI
+
+struct GenreSelectModalView: View {
+    
+    //MARK: - Property Wrappers
+    
+    @StateObject private var viewModel = GenreSelectModalViewModel()
+
+    @State private var inputGenreText = ""
+    @State private var isSearchCompleted: Bool = false
+    
+    //MARK: - Main Body
+    
+    var body: some View {
+        ZStack(alignment: .top) {
+            genreListScrollView
+            headerView
+            applyButtonView
+        }
+        .clipShape(.rect(topLeadingRadius: 20, topTrailingRadius: 20))
+        .padding(.top, 250)
+        .ignoresSafeArea()
+    }
+}
+
+extension GenreSelectModalView {
+    
+    //MARK: - UI Properties
+    
+    private var headerView: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            HStack {
+                Spacer()
+                Button {
+                    //모달 닫기
+                } label: {
+                    Image(.iconCloseModal)
+                }
+            }
+            .padding(.bottom, 20)
+            
+            Text("장르 선택")
+                .applyNapzakFont(.title2Bold20)
+                .foregroundStyle(Color.napzakGrayScale(.gray500))
+                .frame(height: 26)
+                .padding(.bottom, 2)
+            Text("최대 7개 선택")
+                .applyNapzakFont(.caption2Medium12)
+                .foregroundStyle(Color.napzakPrimary(.purple500))
+                .frame(height: 15)
+                .padding(.bottom, 25)
+
+            Group {
+                SearchBar(
+                    placeholder: "어떤 장르의 굿즈인가요? 검색해보세요!",
+                    text: $inputGenreText,
+                    isCompleted: $isSearchCompleted
+                )
+                
+                if !viewModel.selectedGenres.isEmpty {
+                    ChipsContainerView(selectedGenres: $viewModel.selectedGenres)
+                        .frame(height: 29)
+                }
+            }
+            .padding(.bottom, 23)
+        }
+        .padding(.horizontal, 27)
+        .padding(.top, 18)
+        .background(
+            Color.napzakGrayScale(.white)
+                .shadow(color: .black.opacity(0.1), radius: 4)
+        )
+    }
+    
+    private var genreListScrollView: some View {
+        ZStack(alignment: .bottom) {
+            Color.napzakGrayScale(.gray10)
+            
+            ScrollView(showsIndicators: false) {
+                LazyVStack(alignment: .leading) {
+                    ForEach(viewModel.allDummyGenres, id: \.self){ genre in
+                        Button {
+                            viewModel.selectGenre(genre)
+                        } label: {
+                            Text("\(genre.name)")
+                                .applyNapzakFont(.body6Regular14)
+                                .foregroundStyle(viewModel.selectedGenres.contains(genre) ? Color.napzakPrimary(.purple500) : Color.napzakGrayScale(.gray400))
+                                .padding(10)
+                        }
+                    }
+                }
+                .padding(.top, 15)
+                .padding(.bottom, 128)
+                .padding(.leading, 18)
+            }
+            .padding(.top, viewModel.selectedGenres.isEmpty ? 186 : 235 )
+            
+            LinearGradient(
+                gradient: Gradient(colors: [
+                    .white.opacity(0),
+                    .white
+                ]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .frame(height: 270)
+            .allowsHitTesting(false)
+        }
+    }
+    
+    private var applyButtonView: some View {
+        VStack {
+            Spacer()
+            if viewModel.showToast {
+                ToastMessageView(message: "관심 장르는 최대 7개까지만 고를 수 있어요")
+                    .transition(.move(edge: .bottom).combined(with: .opacity))
+                    .padding(.bottom, 30)
+            }
+            Button {
+                //선택한 장르 적용
+            } label: {
+                Text("적용하기")
+                    .applyNapzakFont(.body4Bold14)
+                    .foregroundStyle(Color.napzakGrayScale(.white))
+                    .frame(maxWidth: .infinity)
+            }
+            .background(
+                RoundedRectangle(cornerRadius: 14)
+                    .fill(Color.napzakPrimary(.purple500))
+                    .frame(height: 50)
+            )
+        }
+        .padding(.horizontal, 27)
+        .padding(.bottom, 63)
+        .animation(.easeInOut(duration: 0.3), value: viewModel.showToast)
+    }
+}
+
+#Preview {
+    struct PreviewContainer: View {
+        var body: some View {
+            GenreSelectModalView()
+        }
+    }
+    
+    return PreviewContainer()
+}
