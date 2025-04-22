25ab0b9b0a9f41f6d757c7a96b6d54be18ea8078
OneTen19 <khy000426@naver.com>
2025-04-22 23:00:28 +0900

    [Feat] #18 - ReportReasonMessage struct 생성

--- Napzak-iOS/Napzak-iOS/Global/Literals/StringLiterals.swift
+++ Napzak-iOS/Napzak-iOS/Global/Literals/StringLiterals.swift
@@ -5,4 +5,15 @@
 //  Created by OneTen on 4/22/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct ReportReasonMessage {
+    static let prohibitedProduct = "거래 금지 상품을 판매하고 있어요"
+    static let inappropriateContent = "부적절한 콘텐츠를 포함하고 있어요"
+    static let includefalseInfoOrAd = "허위/과장 정보 및 광고를 포함하고 있어요"
+    static let badManners = "비매너 마켓이에요"
+    static let suspectedFraud = "사기 행위가 의심돼요"
+    static let offensiveLanguage = "욕설/비속어 등 불쾌한 표현을 사용했어요"
+    static let dispute = "거래 과정에서 분쟁이 발생했어요"
+    static let other = "기타 문제가 있어요"
+}
