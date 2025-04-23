35492bf345130ec83117ffa9e54cecd3a617bfb1
JoHyerin <gpfls468@gmail.com>
2025-04-23 16:07:05 +0900

    [Feat] #26 - SearchChip 구현

---
+++ Napzak-iOS/Napzak-iOS/Presentation/Search/Component/SearchChip.swift
@@ -0,0 +1,35 @@
+//
+//  SearchChip.swift
+//  Napzak-iOS
+//
+//  Created by 조혜린 on 4/23/25.
+//
+
+import SwiftUI
+
+struct SearchChip: View {
+    
+    //MARK: - Properties
+    
+    let title: String
+    
+    //MARK: - body
+    
+    var body: some View {
+        Text(title)
+            .applyNapzakFont(.caption1SemiBold12)
+            .foregroundStyle(Color.napzakPrimary(.purple500))
+            .frame(height: 15)
+            .padding(.horizontal, 10)
+            .padding(.vertical, 8)
+            .clipShape(Capsule())
+            .overlay(
+                Capsule()
+                    .stroke(Color.napzakPrimary(.purple500), lineWidth: 1)
+            )
+    }
+}
+
+#Preview {
+    SearchChip(title: "헌터x헌터 룩업")
+}
