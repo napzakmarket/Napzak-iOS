6ad4e37ecdf5895305bea7984d1d91622811be8d
JoHyerin <gpfls468@gmail.com>
2025-04-28 15:23:13 +0900

    [Fix] #32 - 디코딩을 위한 init 함수 수정

--- Napzak-iOS/Napzak-iOS/Global/Models/GenreNameModel.swift
+++ Napzak-iOS/Napzak-iOS/Global/Models/GenreNameModel.swift
@@ -9,6 +9,20 @@ struct GenreNameModel: Identifiable, Hashable {
     let id: Int
     let name: String
     
+    //MARK: - Init
+    
+    ///default init
+    init(id: Int, name: String) {
+        self.id = id
+        self.name = name
+    }
+    
+    ///init for decoding
+    init(dto: GenreDTO) {
+        self.id = dto.genreId
+        self.name = dto.genreName
+    }
+    
     static var sample: GenreNameModel {
         GenreNameModel(id: 1, name: "나루토")
     }
