f3c0f554fd60e8a662b723b88df0fda7ca544f05
OneTen19 <khy000426@naver.com>
2025-04-29 22:34:57 +0900

    [Feat] #38 - API 연결 준비

--- Napzak-iOS/Napzak-iOS/Presentation/Register/ViewModel/RegisterViewModel.swift
+++ Napzak-iOS/Napzak-iOS/Presentation/Register/ViewModel/RegisterViewModel.swift
@@ -19,20 +19,34 @@ final class RegisterViewModel: ObservableObject {
     
     @Published var genreSearchText = ""
     @Published var isCompleted: Bool = false
-    @Published var genreList: [GenreName] = [GenreName(id: 1, name: "나루토"),
-                                                 GenreName(id: 2, name: "원피스"),
-                                                 GenreName(id: 3, name: "드래곤볼"),
-                                                 GenreName(id: 4, name: "명탐정 코난"),
-                                                 GenreName(id: 5, name: "진격의 거인"),
-                                                 GenreName(id: 6, name: "슬램덩크")]
+    @Published var genreList: [GenreName] = []
     
-    
-
     let options = ["미개봉", "아주 좋은 상태", "약간의 사용감", "사용감 있음"]
     let maxPrice: Int = 1_000_000       // 최대 금액 100만원
     let normalMaxDeliveryCharge: Int = 30_000           // 일반 배달 최대 금액 3만원
     let halfMaxDeliveryCharge: Int = 5_000              // 반 값 배달 최대 금액 5000원
     let addPrices = ["+1,000원", "+5,000원", "+10,000원", "+100,000원"]
 
+    init() {
+        fetchGenre(genreSearchText: genreSearchText)
+    }
     
 }
+
+extension RegisterViewModel {
+    func fetchGenre(genreSearchText: String) {
+        if genreSearchText.isEmpty {
+            //TODO: - 전체 목록 API
+            genreList = [GenreName(id: 1, name: "나루토"),
+                              GenreName(id: 2, name: "원피스"),
+                              GenreName(id: 3, name: "드래곤볼"),
+                              GenreName(id: 4, name: "명탐정 코난"),
+                              GenreName(id: 5, name: "진격의 거인"),
+                                 GenreName(id: 6, name: "슬램덩크")]
+        } else {
+            //TODO: - 검색 목록 API
+        }
+    }
+}
+
+
