4653d812947c5ce6c3e5d849e38c8a207384dfcb
Ajin Park <108174693+lalaurrel@users.noreply.github.com>
2025-04-30 01:37:34 +0900

    Refactor: 마켓뷰 정렬필터 장르필터 plainChip 적용

--- Napzak-iOS/Napzak-iOS/Presentation/Market/MarketView.swift
+++ Napzak-iOS/Napzak-iOS/Presentation/Market/MarketView.swift
@@ -9,29 +9,78 @@ import SwiftUI
 
 struct MarketView: View {
     
-    @StateObject private var viewModel = SearchViewModel()
+    @StateObject private var viewModel = MarketViewModel()
+    @State private var isGenreSelectModalPresented = false
+    @State private var isSortModalPresented = false
+    @State private var selectedSortOption: SortOption = .recent
     
     private let productCellWidth = (UIScreen.main.bounds.width - 76) / 2
     private let columns = [GridItem(.flexible(), spacing: 20), GridItem(.flexible())]
     
     var body: some View {
-        VStack(spacing: 0) {
-            navBarView
-            ScrollView(showsIndicators: false) {
-                VStack(spacing: 0) {
-                    profileSectionView
-                    tabAndFilterSectionView
-                    productListView
+        ZStack(alignment: .bottom) {
+            VStack(spacing: 0) {
+                navigationBarView
+                ScrollView(showsIndicators: false) {
+                    VStack(spacing: 0) {
+                        profileSectionView
+                        tabAndFilterSectionView
+                        contentListView
+                    }
                 }
+            }
+            
+            if isGenreSelectModalPresented {
+                Color.black.opacity(0.3)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        withAnimation {
+                            isGenreSelectModalPresented = false
+                        }
+                    }
+                
+                ZStack(alignment: .bottom) {
+                    GenreSelectModalView(
+                        viewModel: GenreSelectModalViewModel(
+                            selectedGenres: viewModel.productFetchOption.genres
+                        ),
+                        isGenreSelectModalPresented: $isGenreSelectModalPresented,
+                        adaptedGenres: $viewModel.productFetchOption.genres
+                    )
+                }
+                .edgesIgnoringSafeArea(.bottom)
+                .transition(.move(edge: .bottom))
+            }
+            
+            if isSortModalPresented {
+                Color.black.opacity(0.3)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        withAnimation {
+                            isSortModalPresented = false
+                        }
+                    }
+                
+                ZStack(alignment: .bottom) {
+                    SortModalView(
+                        isSortModalPresented: $isSortModalPresented,
+                        selectedOption: $selectedSortOption
+                    )
+                }
+                .edgesIgnoringSafeArea(.bottom)
+                .transition(.move(edge: .bottom))
             }
         }
         .ignoresSafeArea(edges: .bottom)
         .navigationBarHidden(true)
+        .animation(.easeInOut, value: isGenreSelectModalPresented)
+        .animation(.easeInOut, value: isSortModalPresented)
+        .onChange(of: viewModel.selectedTabIndex) { _ in
+            viewModel.fetchData()
+        }
     }
-}
-
-extension MarketView {
-    private var navBarView: some View {
+    
+    private var navigationBarView: some View {
         HStack {
             Button {
                 //TODO: - 뒤로가기
@@ -90,32 +139,24 @@ extension MarketView {
                     .applyNapzakFont(.body2SemiBold16)
                     .padding(.top, 42)
 
-                Text("잡덕입니다. 최애는 짱구, 철수, 흰둥이, 긴토키, 히지카타 관련 상품 판매 및 구매 제시 채팅 언제든 환영합니다 :)")
+                Text("잡덕입니다. 최애는 짱구, 철수, 흰둥이, 긴토키, 히지카타 관련 상품 판매 및 구매 제시 채팅 언제든 환영합니다 :)")
                     .foregroundColor(Color.napzakGrayScale(.black))
                     .applyNapzakFont(.caption2Medium12)
                     .multilineTextAlignment(.center)
                     .lineLimit(2)
                     .padding(.top, 6)
+                    .padding(.horizontal, 37)
 
             ScrollView(.horizontal, showsIndicators: false) {
                     HStack(spacing: 5) {
                         ForEach(["산리오", "은혼", "주술회전", "원피스", "귀멸의 칼날", "시카모머시기"], id: \.self) { tag in
-                            Text(tag)
-                                .foregroundColor(Color.napzakPrimary(.purple500))
-                                .applyNapzakFont(.caption1SemiBold12)
-                                .padding(.horizontal, 10)
-                                .padding(.vertical, 8)
-                                .background(Color.clear)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 50)
-                                        .stroke(Color.napzakPrimary(.purple500), lineWidth: 1)
-                                )
-                                .cornerRadius(50)
+                            PlainChip(title: tag)
                         }
                     }
                     .padding(.horizontal, 25)
+                    .padding(.vertical, 2)
                 }
-                .padding(.top, 16)
+                .padding(.top, 17)
                 .padding(.bottom, 17)
             }
         }
@@ -129,11 +170,14 @@ extension MarketView {
                 NZSegmentedControl(selectedTabIndex: $viewModel.selectedTabIndex, tabs: ["팔아요", "구해요", "리뷰"], spacing: 16)
                 
                 if viewModel.selectedTabIndex != 2 {
-                    FilterContainerView(selectedTabIndex: $viewModel.selectedTabIndex,
-                                       selectedGenres: $viewModel.productFetchOption.genres,
-                                       isUnopened: $viewModel.productFetchOption.isUnopened,
-                                       isOnSale: $viewModel.productFetchOption.isOnSale)
-                        .frame(height: 54)
+                    FilterContainerView(
+                        isGenreSelectModalPresented: $isGenreSelectModalPresented,
+                        selectedTabIndex: $viewModel.selectedTabIndex,
+                        selectedGenres: $viewModel.productFetchOption.genres,
+                        isUnopened: $viewModel.productFetchOption.isUnopened,
+                        isOnSale: $viewModel.productFetchOption.isOnSale
+                    )
+                    .frame(height: 54)
                 }
             }
             .padding(.horizontal, 28)
@@ -151,56 +195,73 @@ extension MarketView {
         .clipped()
     }
     
-    private var productListView: some View {
+    private var contentListView: some View {
         VStack(spacing: 0) {
             if viewModel.selectedTabIndex == 2 {
-                Text("")
+                ReviewView
             } else {
-                HStack(alignment: .center, spacing: 3) {
-                    Text("상품")
-                        .foregroundStyle(Color.napzakGrayScale(.gray500))
-                        .applyNapzakFont(.body5SemiBold14)
-                    Text("00개")
-                        .foregroundStyle(Color.napzakPrimary(.purple500))
-                        .applyNapzakFont(.body5SemiBold14)
-                    Spacer()
-                    Button {
-                        //TODO: - 상품 정렬
-                    } label: {
-                        HStack(alignment: .center, spacing: 4) {
-                            Text("최신순")
-                                .foregroundStyle(Color.napzakGrayScale(.gray200))
-                                .applyNapzakFont(.caption1SemiBold12)
-                            Image(.iconArrowDown)
-                                .renderingMode(.template)
-                                .foregroundColor(Color.napzakGrayScale(.gray200))
-                        }
-                    }
-                }
-                .padding(.horizontal, 28)
-                .frame(height: 58)
-
-        LazyVGrid(columns: columns, spacing: 20) {
-            ForEach(viewModel.dummyProducts.indices, id: \.self) { i in
-                ProductItemView(
-                    product: $viewModel.dummyProducts[i],
-                    width: productCellWidth,
-                    shouldToggleInterestState: {
-                        return viewModel.canToggleInterestState(
-                            productID: viewModel.dummyProducts[i].id
-                        )
-                    })
-                    .onTapGesture {
-                        //TODO: - 화면 전환
-                        print("\(viewModel.dummyProducts[i].id)번 상품")
-                    }
-            }
-                }
-                .padding(.horizontal, 28)
+                productListView
             }
         }
         .padding(.bottom, 108)
         .background(Color.napzakGrayScale(.white))
+    }
+    
+    private var productListView: some View {
+        VStack(spacing: 0) {
+            HStack(alignment: .center, spacing: 3) {
+                Text("상품")
+                    .foregroundStyle(Color.napzakGrayScale(.gray500))
+                    .applyNapzakFont(.body5SemiBold14)
+                Text("\(viewModel.getProductCount())개")
+                    .foregroundStyle(Color.napzakPrimary(.purple500))
+                    .applyNapzakFont(.body5SemiBold14)
+                Spacer()
+                Button {
+                    withAnimation {
+                        isSortModalPresented = true
+                    }
+                } label: {
+                    HStack(alignment: .center, spacing: 4) {
+                        Text(selectedSortOption.title)
+                            .foregroundStyle(Color.napzakGrayScale(.gray200))
+                            .applyNapzakFont(.caption1SemiBold12)
+                        Image(.iconArrowDown)
+                            .renderingMode(.template)
+                            .foregroundColor(Color.napzakGrayScale(.gray200))
+                    }
+                }
+            }
+            .padding(.horizontal, 28)
+            .frame(height: 58)
+
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(viewModel.dummyProducts.indices, id: \.self) { i in
+                    ProductItemView(
+                        product: $viewModel.dummyProducts[i],
+                        width: productCellWidth,
+                        shouldToggleInterestState: {
+                            return viewModel.canToggleInterestState(
+                                productID: viewModel.dummyProducts[i].id
+                            )
+                        })
+                        .onTapGesture {
+                            //TODO: - 화면 전환
+                            print("\(viewModel.dummyProducts[i].id)번 상품")
+                        }
+                }
+            }
+            .padding(.horizontal, 28)
+        }
+    }
+    
+    private var ReviewView: some View {
+        VStack(spacing: 16) {
+            Spacer()
+                .frame(height: 100)
+            Spacer()
+        }
+        .frame(maxWidth: .infinity)
     }
 }
 
