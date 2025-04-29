904fda37cd65bce8dca5c92ce49513f2b54bc0b0
joho2022 <josama1997@naver.com>
2025-04-29 22:54:06 +0900

    [Feat] #20 - 배너 액션 및 상품 섹션 Enum 추가, 홈 배너 모델 구현

---
+++ Napzak-iOS/Napzak-iOS/Global/Models/HomeBanner/BannerItem.swift
@@ -0,0 +1,14 @@
+//
+//  BannerItem.swift
+//  Napzak-iOS
+//
+//  Created by 조호근 on 4/28/25.
+//
+
+import Foundation
+
+struct BannerItem: Identifiable {
+    let id: Int
+    let imageURL: String
+    let action: BannerAction
+}
