bc8a729f8e0f8e3672153baa36498b9677ab79d8
JoHyerin <gpfls468@gmail.com>
2025-05-01 02:42:49 +0900

    Refactor: #28 프로필편집뷰 코리 반영

--- Napzak-iOS/Napzak-iOS/Presentation/ProfileEdit/ProfileEditView.swift
+++ Napzak-iOS/Napzak-iOS/Presentation/ProfileEdit/ProfileEditView.swift
@@ -12,25 +12,47 @@ struct ProfileEditView: View {
     @State private var profileDescription: String = ""
     @State private var selectedTags: [String] = []
     @State private var isPrimaryButtonEnabled: Bool = false
-
-    let tags = ["산리오", "운혼", "주술회전", "귀멸의 칼날", "사카모토데이즈", "디즈니/픽사"]
+    @State private var isGenreSelectModalPresented: Bool = false
+    @State private var adaptedGenres: [GenreNameModel] = []
 
     var body: some View {
-        ScrollView {
-            VStack {
-                HeaderView()
-                ProfileImageSection()
-                MarketNameView(isPrimaryButtonEnabled: $isPrimaryButtonEnabled)
-                MarketDescriptionSection(description: $profileDescription)
-                GenreSelectionSection(tags: tags, selectedTags: $selectedTags)
-                ConfirmButton(isEnabled: isPrimaryButtonEnabled)
+        ZStack(alignment: .bottom) {
+            ScrollView {
+                VStack {
+                    headerView
+                    profileImageSection
+                    marketNameView
+                    marketDescriptionSection
+                    genreSelectionSection
+                    confirmButton
+                }
+            }
+            
+            if isGenreSelectModalPresented {
+                Color.black.opacity(0.3)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        withAnimation {
+                            isGenreSelectModalPresented = false
+                        }
+                    }
+                
+                GenreSelectModalView(
+                    viewModel: GenreSelectModalViewModel(
+                        selectedGenres: adaptedGenres
+                    ),
+                    isGenreSelectModalPresented: $isGenreSelectModalPresented,
+                    adaptedGenres: $adaptedGenres
+                )
             }
         }
+        .animation(.easeInOut, value: isGenreSelectModalPresented)
+        .edgesIgnoringSafeArea(.bottom)
     }
 }
 
-struct HeaderView: View {
-    var body: some View {
+extension ProfileEditView {
+    private var headerView: some View {
         HStack {
             Image(.iconBack)
                 .foregroundColor(Color.napzakGrayScale(.gray200))
@@ -43,10 +65,8 @@ struct HeaderView: View {
         }
         .padding(.horizontal)
     }
-}
-
-struct ProfileImageSection: View {
-    var body: some View {
+    
+    private var profileImageSection: some View {
         VStack {
             ZStack {
                 Rectangle()
@@ -62,7 +82,9 @@ struct ProfileImageSection: View {
                     .padding(.top, 180)
                     .offset(x: 5, y: -35)
                 
-                ZStack {
+                Button {
+                    // 사진 편집 기능이 들어갈 예정
+                } label: {
                     Image("edit")
                         .resizable()
                         .scaledToFit()
@@ -74,167 +96,154 @@ struct ProfileImageSection: View {
         }
         .frame(maxWidth: .infinity)
     }
-}
-
-struct MarketNameView: View {
-    @Binding var isPrimaryButtonEnabled: Bool
     
-    var body: some View {
-        VStack(alignment: .leading) {
+    private var marketNameView: some View {
+        VStack(alignment: .leading, spacing: 4) {
             Text("마켓 이름")
                 .applyNapzakFont(.body5SemiBold14)
                 .foregroundColor(Color.napzakGrayScale(.gray400))
-            
             Text("띄어쓰기 없이 한글, 영문, 숫자만 사용할 수 있어요 (최대 20자)")
-                .applyNapzakFont(.caption5Regular10)
-                .foregroundColor(Color.napzakGrayScale(.gray300))
-            
+                           .applyNapzakFont(.caption5Regular10)
+                           .foregroundColor(Color.napzakGrayScale(.gray300))
+                           .padding(.bottom,16)
+
             UsernameInputField(isPrimaryButtonEnabled: $isPrimaryButtonEnabled)
-                .padding(.top, 10)
-
+                           .padding(.top, 10)
+                           .padding(.bottom,20)
         }
         .padding(.horizontal, 20)
         
-        Rectangle()
-            .fill(Color.napzakGrayScale(.gray10))
-            .frame(height: 4)
-            .padding(.bottom, 30)
+        .background(
+            VStack {
+                Spacer()
+                Rectangle()
+                    .fill(Color.napzakGrayScale(.gray10))
+                    .frame(height: 4)
+                    .padding(.bottom, 30)
+            }
+        )
     }
     
-}
-
-struct MarketDescriptionSection: View {
-    @Binding var description: String
+    private var marketDescriptionSection: some View {
+            VStack(alignment: .leading, spacing: 4) {
+               Text("마켓 소개")
+                   .applyNapzakFont(.body5SemiBold14)
+                   .foregroundColor(Color.napzakGrayScale(.gray400))
+               
+               Text("어떤 장르를 좋아하고, 판매하는지! 덕후력을 뽐내는 소개를 작성해주세요")
+                   .applyNapzakFont(.caption5Regular10)
+                   .foregroundColor(Color.napzakGrayScale(.gray300))
+               
+               descriptionEditor
+               
+               Text("\(profileDescription.count)/200")
+                    .applyNapzakFont(.caption4SemiBold10)
+                   .foregroundColor(Color.napzakGrayScale(.gray300))
+                   .frame(maxWidth: .infinity, alignment: .trailing)
+                   .padding(.top, 14)
+                   .padding(.bottom, 30)
+           }
+           .padding(.horizontal, 20)
+           
+           .background(
+               VStack {
+                   Spacer()
+                   Rectangle()
+                       .fill(Color.napzakGrayScale(.gray10))
+                       .frame(height: 4)
+               }
+           )
+       }
     
-    var body: some View {
-        VStack(alignment: .leading) {
-            Text("마켓 소개")
-                .applyNapzakFont(.body5SemiBold14)
-                .foregroundColor(Color.napzakGrayScale(.gray400))
-            
-            Text("어떤 장르를 좋아하고, 판매하는지! 덕후력을 뽐내는 소개를 작성해주세요")
-                .applyNapzakFont(.caption5Regular10)
-                .foregroundColor(Color.napzakGrayScale(.gray300))
-            
-            DescriptionEditor(description: $description, maxLength: 200)
-            
-            Text("\(description.count)/200")
-                .font(.caption)
-                .foregroundColor(.gray)
-                .frame(maxWidth: .infinity, alignment: .trailing)
-                .padding(.top,14)
-        }
-        .padding(.horizontal, 20)
-        
-        Rectangle()
-            .fill(Color.napzakGrayScale(.gray10))
-            .frame(height: 4)
-            .padding(.top, 27)
-    }
-}
-
-struct DescriptionEditor: View {
-    @Binding var description: String
-    let maxLength: Int
-    
-    var body: some View {
+    private var descriptionEditor: some View {
         ZStack(alignment: .topLeading) {
-            TextEditor(text: $description)
+            TextEditor(text: $profileDescription)
+                .applyNapzakFont(.caption2Medium12)
                 .padding(10)
                 .scrollContentBackground(.hidden)
                 .background(Color.napzakGrayScale(.gray50))
                 .clipShape(RoundedRectangle(cornerRadius: 14))
                 .frame(height: 150)
-                .onChange(of: description) { newValue in
-                    if newValue.count > maxLength {
-                        description = String(newValue.prefix(maxLength))
+                .onChange(of: profileDescription) { newValue in
+                    if newValue.count > 200 {
+                        profileDescription = String(newValue.prefix(200))
                     }
                 }
 
-            if description.isEmpty {
+            if profileDescription.isEmpty {
                 Text("어떤 장르를 좋아하고, 판매하는지!\n덕후력을 뽐내는 소개를 작성해주세요")
                     .applyNapzakFont(.caption2Medium12)
                     .foregroundColor(Color.napzakGrayScale(.gray200))
-                    .padding(.vertical,14)
-                    .padding(.horizontal,17)
+                    .padding(.vertical, 14)
+                    .padding(.horizontal, 17)
             }
         }
-        .padding(.top,20)
+        .padding(.top, 20)
     }
-}
-
-struct GenreSelectionSection: View {
-    let tags: [String]
-    @Binding var selectedTags: [String]
     
-    var body: some View {
-        VStack(alignment: .leading) {
+    private var genreSelectionSection: some View {
+        VStack(alignment: .leading, spacing: 4) {
             Text("관심 장르")
                 .applyNapzakFont(.body5SemiBold14)
                 .foregroundColor(Color.napzakGrayScale(.gray400))
+                .padding(.top,27)
             Text("최대 7개 선택 가능")
                 .applyNapzakFont(.caption5Regular10)
                 .foregroundColor(Color.napzakGrayScale(.gray300))
-                
+                .padding(.bottom,16)
             
-            LazyVGrid(columns: [
-                GridItem(.flexible(), spacing: 3),
-                GridItem(.flexible(), spacing: 3),
-                GridItem(.flexible(), spacing: 3)
-            ], alignment: .leading, spacing: 8) {
-                ForEach(["산리오", "은혼", "주술회전", "귀멸의 칼날", "시카모리", "디즈니/픽사"], id: \.self) { tag in
-                    Text(tag)
-                        .foregroundColor(Color.napzakPrimary(.purple500))
-                        .applyNapzakFont(.caption1SemiBold12)
-                        .padding(.horizontal, 10)
-                        .padding(.vertical, 6)
-                        .background(Color.clear)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 38)
-                                .stroke(Color.napzakPrimary(.purple500), lineWidth: 1)
-                        )
-                        .cornerRadius(38)
-                        .frame(maxWidth: .infinity, alignment: .leading)
+            PlainChipContainerView(
+                titles: ["산리오", "은혼", "주술회전", "귀멸의 칼날", "사카모토데이즈", "디즈니/픽사"],
+                action: { title in
+                    if selectedTags.contains(title) {
+                        selectedTags.removeAll { $0 == title }
+                    } else {
+                        if selectedTags.count < 7 {
+                            selectedTags.append(title)
+                        }
+                    }
                 }
-            }
-            .padding(.vertical, 10)
-            .frame(maxWidth: .infinity, alignment: .leading)
-            
-            Button("관심 장르 설정") {}
-                .frame(maxWidth: .infinity)
-                .padding()
-                .background(Color.napzakGrayScale(.gray50))
-                .applyNapzakFont(.body4Bold14)
-                .foregroundColor(Color.napzakGrayScale(.gray400))
-                .cornerRadius(14)
-                .padding(.top, 20)
-        }
-        .padding(.horizontal,27)
-        .padding(.top,27)
-    }
-}
+            )
+            .padding(.bottom,25)
 
-struct ConfirmButton: View {
-    var isEnabled: Bool = false
+            Button {
+                withAnimation {
+                    isGenreSelectModalPresented = true
+                }
+            } label: {
+                Text("관심 장르 설정")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.napzakGrayScale(.gray50))
+                    .applyNapzakFont(.body4Bold14)
+                    .foregroundColor(Color.napzakGrayScale(.gray400))
+                    .cornerRadius(14)
+            }
+        }
+        .padding(.horizontal, 27)
+    }
     
-    var body: some View {
-        Button(action: {}) {
+    private var confirmButton: some View {
+        Button {
+            // 확인 버튼
+        } label: {
             Text("확인")
                 .frame(maxWidth: .infinity)
                 .padding()
-                .background(isEnabled ? Color.napzakPrimary(.purple500) : Color.napzakPrimary(.purple500).opacity(0.5))
+                .background(isPrimaryButtonEnabled ? Color.napzakPrimary(.purple500) : Color.napzakPrimary(.purple500).opacity(0.5))
                 .applyNapzakFont(.body4Bold14)
                 .foregroundColor(Color.napzakGrayScale(.white))
                 .cornerRadius(14)
         }
-        .disabled(!isEnabled)
+        .disabled(!isPrimaryButtonEnabled)
         .padding(.bottom, 63)
-        .padding(.horizontal,27)
-        .padding(.top,10)
+        .padding(.horizontal, 27)
+        .padding(.top, 5)
     }
 }
 
 // MARK: - Preview
+
 struct ProfileEditView_Previews: PreviewProvider {
     static var previews: some View {
         ProfileEditView()
