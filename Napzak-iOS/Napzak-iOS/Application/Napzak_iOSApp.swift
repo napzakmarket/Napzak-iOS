592337dddbf33fb212ef2275f7d14d02041de490
joho2022 <josama1997@naver.com>
2025-05-02 12:22:11 +0900

    [Feat] #41 - Kakao SDK 초기화 및 딥링크 처리 구현

--- Napzak-iOS/Napzak-iOS/Application/Napzak_iOSApp.swift
+++ Napzak-iOS/Napzak-iOS/Application/Napzak_iOSApp.swift
@@ -6,13 +6,26 @@
 //
 
 import SwiftUI
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 struct Napzak_iOSApp: App {
+    
+    init() {
+        let kakaoAppKey = Bundle.main.infoDictionary?["KAKAO_APP_KEY"] as? String ?? ""
+        KakaoSDK.initSDK(appKey: kakaoAppKey)
+    }
+    
     var body: some Scene {
         WindowGroup {
             NZTabBarView()
                 .environmentObject(NavigationRouter())
+                .onOpenURL { url in
+                    if AuthApi.isKakaoTalkLoginUrl(url) {
+                        _ = AuthController.handleOpenUrl(url: url)
+                    }
+                }
         }
     }
 }
