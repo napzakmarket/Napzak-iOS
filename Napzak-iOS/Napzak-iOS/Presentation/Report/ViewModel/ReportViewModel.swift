1aa30c8e4fc1467957514c3d997b26ec7f6627bd
OneTen19 <khy000426@naver.com>
2025-05-04 04:58:34 +0900

    [Chore] #52 - logger category 변경

--- Napzak-iOS/Napzak-iOS/Presentation/Report/ViewModel/ReportViewModel.swift
+++ Napzak-iOS/Napzak-iOS/Presentation/Report/ViewModel/ReportViewModel.swift
@@ -17,7 +17,7 @@ final class ReportViewModel: ObservableObject {
     @Published var reasonExpanded: Bool = false
     @Published var showToast: Bool = false
     
-    private let logger = Logger(subsystem: Bundle.main.bundleIdentifier ?? "Napzak", category: "GenreSelection")
+    private let logger = Logger(subsystem: Bundle.main.bundleIdentifier ?? "Napzak", category: "Report")
     
     let reportDescriptionPlaceholder = "어떤 일이 있었나요? 💬 \n\n자세한 설명일수록 빠른 해결에 도움이 됩니다. \n신고 내용은 비공개로 안전하게 처리되니 안심하세요. \n안전한 거래 공간을 함께 만들어가요!"
     
