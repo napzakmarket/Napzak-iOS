8190b91f94a76a2ce333ff1fb062487f8b8dbcfe
OneTen19 <khy000426@naver.com>
2025-05-04 21:09:02 +0900

    [Add] #46 - PresignedProductResponseDTO

---
+++ Napzak-iOS/Napzak-iOS/Network/Presigned/DTO/Response/PresignedProductResponseDTO.swift
@@ -0,0 +1,16 @@
+//
+//  PresignedResponseDTO.swift
+//  Napzak-iOS
+//
+//  Created by OneTen on 5/4/25.
+//
+
+struct PresignedProductResponseDTO: Codable {
+    let status: Int
+    let message: String
+    let data: PresignedProductUrlsData
+}
+
+struct PresignedProductUrlsData: Codable {
+    let product: [String: String]
+}
