64f72468554feb0a18466dda7eccac52fb89c9b2
OneTen19 <khy000426@naver.com>
2025-05-04 21:17:37 +0900

    [Feat] #46 - PresignedService

--- Napzak-iOS/Napzak-iOS/Network/Presigned/PresignedService.swift
+++ Napzak-iOS/Napzak-iOS/Network/Presigned/PresignedService.swift
@@ -10,11 +10,20 @@ import Foundation
 import Moya
 
 protocol PresignedServiceProtocol {
-    
+    func getPresignedURL(imageNameList: [String]) async -> Result<PresignedProductResponseDTO, NetworkError>
+    func putPresignedURL(url: String, imageData: Data) async -> Result<Void, NetworkError>
 }
 
 final class PresignedService: BaseService, PresignedServiceProtocol {
     
-//    let provider = MoyaProvider<PresignedAPI>.init(plugins: [MoyaPlugin()])
+    let provider = MoyaProvider<PresignedAPI>.init(plugins: [MoyaPlugin()])
+
+    func getPresignedURL(imageNameList: [String]) async -> Result<PresignedProductResponseDTO, NetworkError> {
+        return await request(provider, .getPresignedProductURL(imageNameList: imageNameList))
+    }
+
+    func putPresignedURL(url: String, imageData: Data) async -> Result<Void, NetworkError> {
+        return await request(provider, .putPresignedProductURL(url: url, imageData: imageData))
+    }
     
 }
