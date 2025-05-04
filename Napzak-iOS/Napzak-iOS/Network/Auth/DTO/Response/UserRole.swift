a28095b1781e3d4b52b86af8db3fa957b0fd0c48
joho2022 <josama1997@naver.com>
2025-05-05 01:43:33 +0900

    [Del] #41 - Auth User 모델 삭제

--- Napzak-iOS/Napzak-iOS/Network/Auth/DTO/Response/UserRole.swift
+++ Napzak-iOS/Napzak-iOS/Network/Auth/DTO/Response/UserRole.swift
@@ -9,12 +9,12 @@ import Foundation
 
 enum UserRole: String, Decodable {
     case store = "STORE"
-    case pending = "PENDING"
+    case onboarding = "ONBOARDING"
     
     var description: String {
         switch self {
         case .store: return "스토어"
-        case .pending: return "미완료"
+        case .onboarding: return "온보딩 미완료"
         }
     }
 }
