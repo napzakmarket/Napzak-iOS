27c092e408ba8e520a2504b814a67857f8fbf45d
OneTen19 <khy000426@naver.com>
2025-05-05 00:41:55 +0900

    [Feat] #46 - RegisterAPI

--- Napzak-iOS/Napzak-iOS/Network/Register/RegisterAPI.swift
+++ Napzak-iOS/Napzak-iOS/Network/Register/RegisterAPI.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+import Moya
+
+enum RegisterAPI {
+    case sellRegister(registerItem: SellRegisterRequestDTO)
+}
+
+extension RegisterAPI: BaseTargetType {
+    var headerType: HeaderType {
+        switch self {
+        case .sellRegister(let registerItem):
+            return .accessTokenHeader
+        }
+    }
+
+    var path: String {
+        switch self {
+        case .sellRegister:
+            return "products/sell"
+        }
+    }
+
+    var method: Moya.Method {
+        switch self {
+        case .sellRegister(let registerItem):
+            return .post
+        }
+    }
+
+    var task: Moya.Task {
+        switch self {
+        case .sellRegister:
+            return .requestPlain
+        }
+    }
+
+}
