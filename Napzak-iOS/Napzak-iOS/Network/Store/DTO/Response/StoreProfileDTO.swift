e86e5cccd90f941bce7095d83ca96e232c9e73f8
Ajin Park <108174693+lalaurrel@users.noreply.github.com>
2025-05-06 01:12:53 +0900

    refactor: #48 dto 이름 변경

--- Napzak-iOS/Napzak-iOS/Network/Store/DTO/Response/StoreProfileDTO.swift
+++ Napzak-iOS/Napzak-iOS/Network/Store/DTO/Response/StoreProfileDTO.swift
@@ -1,11 +1,11 @@
 //
-//  StoreDTO.swift
+//  StoreProfileDTO.swift
 //  Napzak-iOS
 //
 //  Created by 어진 on 5/3/25.
 //
 
-struct StoreDTO: Decodable {
+struct StoreProfileDTO: Decodable {
     let storeId: Int
     let storeNickname: String
     let storePhoto: String
