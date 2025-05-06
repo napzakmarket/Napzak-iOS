060735ccd1a47bd4365f963e2a4853c88c895768
JoHyerin <gpfls468@gmail.com>
2025-05-07 03:24:31 +0900

    [Add] #42 - ProductCondition enum 추가

---
+++ Napzak-iOS/Napzak-iOS/Global/Enums/ProductCondition.swift
@@ -0,0 +1,13 @@
+//
+//  ProductCondition.swift
+//  Napzak-iOS
+//
+//  Created by 조혜린 on 5/6/25.
+//
+
+enum ProductCondition: String {
+    case new = "NEW"
+    case likeNew = "LIKE_NEW"
+    case slightlyUsed = "SLIGHTLY_USED"
+    case used = "USED"
+}
