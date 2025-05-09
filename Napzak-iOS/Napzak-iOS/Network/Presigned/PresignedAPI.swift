d362b9d13fd2152eacab8ad48c8d5e05e0a43706
OneTen19 <khy000426@naver.com>
2025-05-10 01:50:01 +0900

    [Chore] #46 - 불필요한 코드 제거

--- Napzak-iOS/Napzak-iOS/Network/Presigned/PresignedAPI.swift
+++ Napzak-iOS/Napzak-iOS/Network/Presigned/PresignedAPI.swift
@@ -19,7 +19,7 @@ extension PresignedAPI: BaseTargetType {
     // 발급받은 PresignedURL에 PUT 요청을 보내야 하기 때문에 BASE URL을 사용하지 않음
     var baseURL: URL {
         switch self {
-        case .getPresignedProductURL(let imageNameList):
+        case .getPresignedProductURL:
             guard let urlString = Bundle.main.infoDictionary?["BASE_URL"] as? String,
                   let url = URL(string: urlString) else {
                 fatalError("🚨Base URL을 찾을 수 없습니다🚨")
@@ -32,36 +32,36 @@ extension PresignedAPI: BaseTargetType {
     
     var headerType: HeaderType {
         switch self {
-        case .getPresignedProductURL(let imageNameList):
+        case .getPresignedProductURL:
                 .noneHeader
-        case .putPresignedProductURL(let url, let imageData):
+        case .putPresignedProductURL:
                 .noneHeader
         }
     }
     
     var headers: [String : String]? {
         switch self {
-        case .getPresignedProductURL(let imageNameList):
+        case .getPresignedProductURL:
             return nil
-        case .putPresignedProductURL(let url, let imageData):
+        case .putPresignedProductURL:
             return ["Content-Type": "image/jpeg"]
         }
     }
 
     var path: String {
         switch self {
-        case .getPresignedProductURL(let imageNameList):
+        case .getPresignedProductURL:
             return "presigned-url/product"
-        case .putPresignedProductURL(let url, let imageData):
+        case .putPresignedProductURL:
             return ""
         }
     }
 
     var method: Moya.Method {
         switch self {
-        case .getPresignedProductURL(let imageNameList):
+        case .getPresignedProductURL:
             return .get
-        case .putPresignedProductURL(let url, let imageData):
+        case .putPresignedProductURL:
             return .put
         }
     }
