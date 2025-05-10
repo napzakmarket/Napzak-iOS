1a6b41cddf85267a3a66c18c0cd5ce956f46de85
OneTen19 <khy000426@naver.com>
2025-05-11 02:50:45 +0900

    [Add] #64 - SettingViewModel

---
+++ Napzak-iOS/Napzak-iOS/Presentation/Mypage/ViewModel/SettingViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  SettingViewModel.swift
+//  Napzak-iOS
+//
+//  Created by OneTen on 5/11/25.
+//
+
+import SwiftUI
+import os
+
+final class SettingViewModel: ObservableObject {
+    private let logger = Logger(subsystem: Bundle.main.bundleIdentifier ?? "Napzak", category: "SettingView")
+
+    
+    
+}
