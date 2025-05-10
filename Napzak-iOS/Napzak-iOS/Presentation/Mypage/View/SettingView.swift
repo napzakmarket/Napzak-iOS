0ad78957fe095d18f59918a9609404c1b2cb529d
OneTen19 <khy000426@naver.com>
2025-05-11 02:48:41 +0900

    [Feat] #64 - SettingView UI

--- Napzak-iOS/Napzak-iOS/Presentation/Mypage/View/SettingView.swift
+++ Napzak-iOS/Napzak-iOS/Presentation/Mypage/View/SettingView.swift
@@ -8,23 +8,61 @@
 import SwiftUI
 
 struct SettingView: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    @State var logoutButtonTapped: Bool = false
+    
     var body: some View {
-        VStack(spacing: 0){
-            SettingViewHeader
+        ZStack {
+            VStack(spacing: 0){
+                settingViewHeader
+                serviceInfo
+                separator
+                logoutButton
+                separator
+                withDrawButton
+                Spacer()
+            }
             
-            Spacer()
+            if logoutButtonTapped {
+                Color.napzakTransparency(.transBlack)
+                    .onTapGesture {
+                        withAnimation {
+                            logoutButtonTapped = false
+                        }
+                    }
+                    .transition(.opacity)
+                    .zIndex(1)
+                
+                NZAlertView(
+                    message: "로그아웃 하시겠어요?",
+                    confirmText: "예",
+                    cancelText: "아니요",
+                    onConfirm: {
+                        print("로그아웃 처리")
+                        logoutButtonTapped = false
+                    },
+                    onCancel: {
+                        logoutButtonTapped = false
+                    }
+                )
+                .padding(.bottom, 120)
+                .padding(.horizontal, 45)
+                .zIndex(2)
+            }
         }
         .ignoresSafeArea()
+        .background(Color.napzakGrayScale(.gray10))
         
     }
 }
 
 extension SettingView {
-    private var SettingViewHeader: some View {
+    private var settingViewHeader: some View {
         VStack(alignment: .leading) {
             Button {
-                //Todo: - 네비게이션 pop
-                print("backButton tapped")
+                // 네비게이션 pop으로 교체
+                dismiss
             } label: {
                 HStack(alignment: .center, spacing: 0) {
                     Image(.iconBack)
@@ -43,9 +81,140 @@ extension SettingView {
             Divider()
         }
         .frame(height: 100)
+        .padding(.bottom, 28)
+        .background(.white)
+    }
+    
+    private var serviceInfo: some View {
+        VStack(alignment: .leading, spacing: 0){
+            Text("서비스 정보")
+                .applyNapzakFont(.body6Regular14)
+                .foregroundStyle(Color.napzakGrayScale(.gray400))
+                .frame(height: 18)
+                .padding(.leading, 28)
+                .padding(.bottom, 28)
+            
+            Button {
+                // 공지사항 이동
+                print("공지사항 이동")
+            } label: {
+                HStack {
+                    Text("공지사항")
+                        .applyNapzakFont(.body1Bold16)
+                        .foregroundStyle(Color.napzakGrayScale(.gray400))
+                        .frame(height: 20)
+                    Spacer()
+                    Image(.arrowRight)
+                        .resizable()
+                        .frame(width: 6, height: 10)
+                }
+            }
+            .padding(.horizontal, 28)
+            .padding(.bottom, 20)
+            
+            Button {
+                // 이용약관 이동
+                print("이용약관 이동")
+            } label: {
+                HStack {
+                    Text("이용약관")
+                        .applyNapzakFont(.body1Bold16)
+                        .foregroundStyle(Color.napzakGrayScale(.gray400))
+                        .frame(height: 20)
+                    Spacer()
+                    Image(.arrowRight)
+                        .resizable()
+                        .frame(width: 6, height: 10)
+                }
+            }
+            .padding(.horizontal, 28)
+            .padding(.bottom, 20)
+            
+            Button {
+                // 개인정보 처리방침 이동
+                print("개인정보 처리방침 이동")
+            } label: {
+                HStack {
+                    Text("개인정보 처리방침")
+                        .applyNapzakFont(.body1Bold16)
+                        .foregroundStyle(Color.napzakGrayScale(.gray400))
+                        .frame(height: 20)
+                    Spacer()
+                    Image(.arrowRight)
+                        .resizable()
+                        .frame(width: 6, height: 10)
+                }
+            }
+            .padding(.horizontal, 28)
+            .padding(.bottom, 20)
+            
+            Button {
+                // 버전 정보 이동
+                print("버전 정보 이동")
+            } label: {
+                HStack {
+                    Text("버전 정보")
+                        .applyNapzakFont(.body1Bold16)
+                        .foregroundStyle(Color.napzakGrayScale(.gray400))
+                        .frame(height: 20)
+                    Spacer()
+                    Image(.arrowRight)
+                        .resizable()
+                        .frame(width: 6, height: 10)
+                }
+            }
+            .padding(.horizontal, 28)
+            .padding(.bottom, 28)
+        }
+        .background(.white)
+    }
+    
+    private var separator: some View {
+        Rectangle()
+            .fill(Color.napzakGrayScale(.gray10))
+            .frame(height: 7)
+    }
+    
+    private var logoutButton: some View {
+        HStack(alignment: .center, spacing: 0) {
+            Button {
+                logoutButtonTapped = true
+            } label: {
+                Text("로그아웃")
+                    .applyNapzakFont(.body1Bold16)
+                    .foregroundStyle(Color.napzakPrimary(.purple500))
+                    .frame(height: 20)
+            }
+            .padding(.leading, 28)
+            .padding(.vertical, 28)
+            
+            Spacer()
+        }
+        .background(.white)
+    }
+    
+    private var withDrawButton: some View {
+        HStack(alignment: .center, spacing: 0) {
+            Button {
+                // 탈퇴 뷰 이동
+                print("탈퇴 뷰 이동")
+            } label: {
+                Text("탈퇴하기")
+                    .applyNapzakFont(.body1Bold16)
+                    .foregroundStyle(.red)
+                    .frame(height: 20)
+            }
+            .padding(.leading, 28)
+            .padding(.vertical, 28)
+            
+            Spacer()
+        }
+        .background(.white)
     }
 }
 
 #Preview {
     SettingView()
 }
+
+
