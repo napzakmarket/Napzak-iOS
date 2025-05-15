ac6b5151e739a9dcc6afff4e7b3f7227e37c2bdb
JoHyerin <gpfls468@gmail.com>
2025-05-15 22:22:15 +0900

    [Fix] #93 - 마켓 뷰 탭 변경시 스크롤 위지 조정

--- Napzak-iOS/Napzak-iOS/Presentation/Market/MarketView.swift
+++ Napzak-iOS/Napzak-iOS/Presentation/Market/MarketView.swift
@@ -19,6 +19,7 @@ struct MarketView: View {
     @State private var isSortModalPresented = false
     @State private var selectedSortOption: SortOption = .recent
     @State private var isReportModalPresented = false
+    @State private var scrollToTopTrigger: Bool = false
     
     private let productCellWidth = (UIScreen.main.bounds.width - 76) / 2
     private let columns = [GridItem(.flexible(), spacing: 20), GridItem(.flexible())]
@@ -102,12 +103,14 @@ struct MarketView: View {
         .onChange(of: viewModel.selectedTabIndex) { _ in
             Task {
                 await viewModel.fetchProducts()
+                scrollToTopTrigger.toggle()
             }
         }
         .onChange(of: selectedSortOption) { newValue in
             viewModel.productFetchOption.sortOption = newValue
             Task {
                 await viewModel.fetchProducts()
+                scrollToTopTrigger.toggle()
             }
         }
         .onAppear {
@@ -147,12 +150,20 @@ struct MarketView: View {
     }
     
     private var mainScrollView: some View {
-        ScrollView {
-            LazyVStack(spacing: 0, pinnedViews: [.sectionHeaders]) {
+        ScrollViewReader { proxy in
+            ScrollView {
                 profileSectionView
-                Section(header: tabAndFilterSectionView) {
-                    contentListView
+                Color.clear
+                    .frame(height: 0)
+                    .id("top")
+                LazyVStack(spacing: 0, pinnedViews: [.sectionHeaders]) {
+                    Section(header: tabAndFilterSectionView) {
+                        contentListView
+                    }
                 }
+            }
+            .onChange(of: scrollToTopTrigger) { _ in
+                proxy.scrollTo("top")
             }
         }
     }
