b24f036cdf5b64144fe912096c386747f7d17e44
OneTen19 <khy000426@naver.com>
2025-05-15 21:02:28 +0900

    [Chore] #92 - Conflict 해결

--- Napzak-iOS/Napzak-iOS/Global/Extensions/View+.swift
+++ Napzak-iOS/Napzak-iOS/Global/Extensions/View+.swift
@@ -22,6 +22,8 @@ extension View {
     
     func swipePopGestureDisabled() -> some View {
         modifier(SwipePopGestureDisabledViewModifier())
+    }
+    
     func dismissKeyboard() {
         UIApplication.shared
             .sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
