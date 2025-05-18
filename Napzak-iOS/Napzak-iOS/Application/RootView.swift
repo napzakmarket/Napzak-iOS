fc0d807e8546e6c97bccbf986785e9454e24f825
joho2022 <josama1997@naver.com>
2025-05-18 12:38:56 +0900

    [Feat] #106 - 스플래시 화면 노출 시간 연장

--- Napzak-iOS/Napzak-iOS/Application/RootView.swift
+++ Napzak-iOS/Napzak-iOS/Application/RootView.swift
@@ -12,16 +12,34 @@ struct RootView: View {
     @StateObject private var navigationRouter = NavigationRouter()
     @StateObject private var tabRouter = TabRouter()
     @StateObject private var authManager = AuthManager.shared
+    @State private var isShowingSplash = true
     
     var body: some View {
-        if authManager.isAuthenticated && !authManager.needsOnboarding {
-            NZTabBarView()
-                .environmentObject(navigationRouter)
-                .environmentObject(tabRouter)
-        } else {
-            LoginView()
-                .environmentObject(authRouter)
-        }    
+        Group {
+            if isShowingSplash {
+                SplashView()
+                    .transition(.opacity)
+            } else if authManager.isAuthenticated && !authManager.needsOnboarding {
+                NZTabBarView()
+                    .environmentObject(navigationRouter)
+                    .environmentObject(tabRouter)
+            } else {
+                LoginView()
+                    .environmentObject(authRouter)
+            }
+        }
+        .onAppear {
+            isShowingSplash = true
+            
+            Task {
+                try? await Task.sleep(for: .seconds(2.5))
+                await MainActor.run {
+                    withAnimation(.easeInOut(duration: 0.3)) {
+                        isShowingSplash = false
+                    }
+                }
+            }
+        }
     }
 }
 
