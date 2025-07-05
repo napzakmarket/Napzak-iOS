639e1e3057f2885e9b77d66ba05b94c1a9fc1dcf
OneTen19 <khy000426@naver.com>
2025-07-05 21:20:22 +0900

    [Add] #131 - SpinnerLoading Lottie

---
+++ Napzak-iOS/Napzak-iOS/Global/Components/SpinnerLoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  SpinnerLoadingView.swift
+//  Napzak-iOS
+//
+//  Created by OneTen on 7/5/25.
+//
+
+import SwiftUI
+
+struct SpinnerLoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpinnerLoadingView()
+}
