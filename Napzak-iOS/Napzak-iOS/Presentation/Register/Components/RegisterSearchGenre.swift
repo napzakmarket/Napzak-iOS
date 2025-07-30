11fffe86d49fc61b93bb7d3d5dfdae60fc197b3e
OneTen19 <khy000426@naver.com>
2025-07-30 14:25:24 +0900

    [Fix] #150 - 등록 장르 검색 결과 없을 시 캐릭터 아이콘 추가

--- Napzak-iOS/Napzak-iOS/Presentation/Register/Components/RegisterSearchGenre.swift
+++ Napzak-iOS/Napzak-iOS/Presentation/Register/Components/RegisterSearchGenre.swift
@@ -118,6 +118,10 @@ extension RegisterSearchGenre {
                 .padding(.leading, 18)
             } else {
                 VStack(alignment: .center, spacing: 0){
+                    Image(.iconEmptySearchGenreIos)
+                        .padding(.top, 60)
+                        .padding(.bottom, 20)
+                    
                     Text("죄송해요, 찾으시는 장르가 아직 없네요\n원하시는 장르를 알려주시면 빠르게 추가할게요!")
                         .applyNapzakFont(.body6Regular14)
                         .fixedSize(horizontal: false, vertical: true)
@@ -125,7 +129,6 @@ extension RegisterSearchGenre {
                         .frame(height: 36)
                         .lineLimit(2)
                         .multilineTextAlignment(.center)
-                        .padding(.top, 40)
                         .padding(.horizontal, 57)
                     
                     Button {
@@ -143,11 +146,23 @@ extension RegisterSearchGenre {
                         RoundedRectangle(cornerRadius: 9)
                             .stroke(Color.napzakPrimary(.purple500), lineWidth: 1)
                     }
-                    .padding(.top, 34)
+                    .padding(.top, 19)
                 }
             }
         }
         .padding(.top, 260)
     }
     
+}
+
+#Preview {
+    @StateObject var registerRouter = RegisterNavigationRouter()
+    @State var genreSearchText: String = ""
+    @State var isCompleted: Bool = false
+    @State var genreList: [GenreNameModel] = []
+    @State var genre: String = ""
+    @State var genreId: Int?
+
+    
+    RegisterSearchGenre(registerRouter: registerRouter, genreSearchText: $genreSearchText, isCompleted: $isCompleted, genreList: $genreList, genre: $genre, genreId: $genreId)
 }
