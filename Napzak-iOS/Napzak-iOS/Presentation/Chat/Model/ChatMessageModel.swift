4adc4ce7437727706fc9a7c6be2ec5816848efb0
JoHyerin <gpfls468@gmail.com>
2025-08-02 22:53:05 +0900

    [Chore] #149 - 자잘한 코드 수정

--- Napzak-iOS/Napzak-iOS/Presentation/Chat/Model/ChatMessageModel.swift
+++ Napzak-iOS/Napzak-iOS/Presentation/Chat/Model/ChatMessageModel.swift
@@ -5,186 +5,40 @@
 //  Created by 조혜린 on 6/30/25.
 //
 
-enum ChatMessageType: String {
-    case text = "TEXT"
-    case image = "IMAGE"
-    case product = "PRODUCT"
-    case system = "SYSTEM"
-    case date = "DATE"
-}
-
-enum ChatMetaDataType {
-    case image(ImageMeta)
-    case product(ProductMeta)
-    case system(SystemMeta)
-    case date(DateMeta)
-}
-
-enum SystemMetaType: String {
-    case leave = "LEAVE"
-    case reported = "REPORTED"
-    case withdrawn = "WITHDRAWN"
-}
-
 struct ChatMessageModel: Identifiable {
     let id: Int
-    let senderId: Int
+    let senderId: Int?
     let type: ChatMessageType
     let content: String?
-    let metaData: ChatMetaDataType?
+    let metadata: ChatMetaDataType?
     let createdAt: String
-    let isFirstChat: Bool
+    let isProfileNeeded: Bool
     let isMessageOwner: Bool
     let isRead: Bool
-}
-
-struct ImageMeta {
-    let type: ChatMessageType
-    let imageUrls: [String]
-}
-
-struct ProductMeta {
-    let type: ChatMessageType
-    let tradeType: TradeType
-    let productId: Int
-    let genreName: String
-    let title: String
-    let price: Int
-}
-
-struct SystemMeta {
-    let type: SystemMetaType
-    let content: String
-}
-
-struct DateMeta {
-    let type: ChatMessageType
-    let date: String
-}
-
-extension ChatMessageModel {
-    static let mock: [ChatMessageModel] = [
-        ChatMessageModel(
-            id: 0,
-            senderId: 3,
-            type: .date,
-            content: nil,
-            metaData: .date(
-                DateMeta(type: .date, date: "2025년 4월 30일")
-            ),
-            createdAt: "오전 7:30",
-            isFirstChat: false,
-            isMessageOwner: false,
-            isRead: true
-        ),
-        ChatMessageModel(
-            id: 1,
-            senderId: 2,
-            type: .product,
-            content: nil,
-            metaData: .product(
-                ProductMeta(
-                    type: .product,
-                    tradeType: .sell,
-                    productId: 0,
-                    genreName: "은혼",
-                    title: "은혼 긴토키 히지카타 룩업",
-                    price: 123000
-                )
-            ),
-            createdAt: "오전 7:30",
-            isFirstChat: false,
-            isMessageOwner: false,
-            isRead: true
-        ),
-        ChatMessageModel(
-            id: 2,
-            senderId: 0,
-            type: .text,
-            content: "구매할래요",
-            metaData: nil,
-            createdAt: "오전 7:30",
-            isFirstChat: true,
-            isMessageOwner: false,
-            isRead: false
-        ),
-        ChatMessageModel(
-            id: 3,
-            senderId: 0,
-            type: .text,
-            content: "좀 애매하긴 해",
-            metaData: nil,
-            createdAt: "오전 7:30",
-            isFirstChat: false,
-            isMessageOwner: false,
-            isRead: true
-        ),
-        ChatMessageModel(
-            id: 4,
-            senderId: 0,
-            type: .text,
-            content: "?",
-            metaData: nil,
-            createdAt: "오전 7:30",
-            isFirstChat: false,
-            isMessageOwner: true,
-            isRead: true
-        ),
-        ChatMessageModel(
-            id: 5,
-            senderId: 0,
-            type: .text,
-            content: "뭐야 가세요;; 뭐야 가세요;; 뭐야 가세요;; 뭐야 가세요;; 뭐야 가세요;; 뭐야 가세요;; 뭐야 가세요;; 뭐야 가세요;; 뭐야 가세요;; 뭐야 가세요;; 뭐야 가세요;; 뭐야 가세요;;",
-            metaData: nil,
-            createdAt: "오전 7:30",
-            isFirstChat: false,
-            isMessageOwner: true,
-            isRead: true
-        ),
-        ChatMessageModel(
-            id: 6,
-            senderId: 3,
-            type: .date,
-            content: nil,
-            metaData: .date(
-                DateMeta(type: .date, date: "2025년 4월 31일")
-            ),
-            createdAt: "오전 7:30",
-            isFirstChat: false,
-            isMessageOwner: false,
-            isRead: true
-        ),
-        ChatMessageModel(
-            id: 7,
-            senderId: 1,
-            type: .image,
-            content: nil,
-            metaData: .image(
-                ImageMeta(
-                    type: .image,
-                    imageUrls: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOrPQwOTaU_L8EIFpWzLjgiUHc3CcmGEq84A&s"]
-                )
-            ),
-            createdAt: "오전 7:30",
-            isFirstChat: true,
-            isMessageOwner: false,
-            isRead: true
-        ),
-        ChatMessageModel(
-            id: 8,
-            senderId: 1,
-            type: .image,
-            content: nil,
-            metaData: .image(
-                ImageMeta(
-                    type: .image,
-                    imageUrls: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOrPQwOTaU_L8EIFpWzLjgiUHc3CcmGEq84A&s"]
-                )
-            ),
-            createdAt: "오전 7:30",
-            isFirstChat: false,
-            isMessageOwner: true,
-            isRead: true
-        )
-    ]
+    
+    ///default init
+    init(id: Int, senderId: Int?, type: ChatMessageType, content: String?, metaData: ChatMetaDataType?, createdAt: String, isProfileNeeded: Bool, isMessageOwner: Bool, isRead: Bool) {
+        self.id = id
+        self.senderId = senderId
+        self.type = type
+        self.content = content
+        self.metadata = metaData
+        self.createdAt = createdAt
+        self.isProfileNeeded = isProfileNeeded
+        self.isMessageOwner = isMessageOwner
+        self.isRead = isRead
+    }
+    
+    ///init for decoding
+    init(dto: ChatMessageNormalDTO) {
+        self.id = dto.messageId
+        self.senderId = dto.senderId
+        self.type = dto.type
+        self.content = dto.content
+        self.metadata = dto.metadata
+        self.createdAt = dto.createdAt
+        self.isProfileNeeded = dto.isProfileNeeded
+        self.isMessageOwner = dto.isMessageOwner
+        self.isRead = dto.isRead
+    }
 }
--- Napzak-iOS/Napzak-iOS/Presentation/Chat/Model/ChatRoomModel.swift
+++ Napzak-iOS/Napzak-iOS/Presentation/Chat/Model/ChatRoomModel.swift
@@ -13,72 +13,27 @@ struct ChatRoomModel: Identifiable {
     let lastMessageAt: String
     let unreadCount: Int
     let opponentStorePhoto: String
-}
+    let isMessageAllowed: Bool? = nil
 
-extension ChatRoomModel {
-    static let mock: [ChatRoomModel] = [
-        ChatRoomModel(
-            id: 0,
-            opponentNickname: "납자기",
-            isOpponentWithdrawn: false,
-            lastMessage: "(이미지)",
-            lastMessageAt: "오전 1:38",
-            unreadCount: 8,
-            opponentStorePhoto: ""
-        ),
-        ChatRoomModel(
-            id: 1,
-            opponentNickname: "우사기",
-            isOpponentWithdrawn: false,
-            lastMessage: "사용자가 채팅방을 나갔습니다.",
-            lastMessageAt: "오전 1:38",
-            unreadCount: 0,
-            opponentStorePhoto: ""
-        ),
-        ChatRoomModel(
-            id: 2,
-            opponentNickname: "(탈퇴한 사용자) 마이린",
-            isOpponentWithdrawn: true,
-            lastMessage: "저기요?",
-            lastMessageAt: "오전 1:38",
-            unreadCount: 1000,
-            opponentStorePhoto: ""
-        ),
-        ChatRoomModel(
-            id: 3,
-            opponentNickname: "정재현",
-            isOpponentWithdrawn: false,
-            lastMessage: "구매?",
-            lastMessageAt: "오전 1:38",
-            unreadCount: 11,
-            opponentStorePhoto: ""
-        ),
-        ChatRoomModel(
-            id: 4,
-            opponentNickname: "토도로키",
-            isOpponentWithdrawn: false,
-            lastMessage: "사용자가 채팅방을 나갔습니다.",
-            lastMessageAt: "오전 1:38",
-            unreadCount: 8,
-            opponentStorePhoto: ""
-        ),
-        ChatRoomModel(
-            id: 5,
-            opponentNickname: "납작아요",
-            isOpponentWithdrawn: false,
-            lastMessage: "안녕하세요~ 안녕하세요~ 안녕하세요~ 안녕하세요~ 안녕하세요~",
-            lastMessageAt: "오전 1:38",
-            unreadCount: 8,
-            opponentStorePhoto: ""
-        ),
-        ChatRoomModel(
-            id: 6,
-            opponentNickname: "박명수",
-            isOpponentWithdrawn: false,
-            lastMessage: ".",
-            lastMessageAt: "오전 1:38",
-            unreadCount: 333,
-            opponentStorePhoto: "https://i.pinimg.com/736x/86/e8/c9/86e8c92b974b7d21a84a2af1b2650143.jpg"
-        )
-    ]
+    ///default init
+    init(id: Int, opponentNickname: String, isOpponentWithdrawn: Bool, lastMessage: String, lastMessageAt: String, unreadCount: Int, opponentStorePhoto: String) {
+        self.id = id
+        self.opponentNickname = opponentNickname
+        self.isOpponentWithdrawn = isOpponentWithdrawn
+        self.lastMessage = lastMessage
+        self.lastMessageAt = lastMessageAt
+        self.unreadCount = unreadCount
+        self.opponentStorePhoto = opponentStorePhoto
+    }
+    
+    ///init for decoding
+    init(dto: ChatRoomDTO) {
+        self.id = dto.roomId
+        self.opponentNickname = dto.opponentNickname
+        self.isOpponentWithdrawn = dto.isOpponentWithdrawn
+        self.lastMessage = dto.lastMessage
+        self.lastMessageAt = dto.lastMessageAt
+        self.unreadCount = dto.unreadCount
+        self.opponentStorePhoto = dto.opponentStorePhoto
+    }
 }
