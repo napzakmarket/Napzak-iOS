769ead35f83b10c293786b8a520ed69f0c58b2ad
JoHyerin <gpfls468@gmail.com>
2025-08-02 22:58:44 +0900

    [Feat] #149 - 채팅 관련 API 함수 구현

--- Napzak-iOS/Napzak-iOS/Network/Chat/ChatAPI.swift
+++ Napzak-iOS/Napzak-iOS/Network/Chat/ChatAPI.swift
@@ -14,9 +14,14 @@ enum BaseURLType {
 }
 
 enum ChatAPI {
-    case getChatInfo(productId: Int)
+    case getChatInfo(productId: Int, roomId: Int?)
     case postCreateChatRoom(requestBody: ChatRoomCreateRequestDTO)
     case patchEnterChatRoom(roomId: Int)
+    case getChatMessages(roomId: Int)
+    case getChatRooms(deviceToken: String?)
+    case patchLeaveChatRoom(roomId: Int)
+    case patchExitChatRoom(roomId: Int)
+    case getChatRoomIds
 }
 
 extension ChatAPI: BaseTargetType {
@@ -55,18 +60,26 @@ extension ChatAPI: BaseTargetType {
     
     var path: String {
         switch self {
-        case .getChatInfo(let productId):
+        case .getChatInfo(productId: let productId, _):
             return "products/chat/\(productId)"
-        case .postCreateChatRoom:
+        case .postCreateChatRoom, .getChatRooms:
             return "rooms"
         case .patchEnterChatRoom(let roomId):
             return "rooms/\(roomId)/enter"
+        case .getChatMessages(let roomId):
+            return "rooms/\(roomId)/messages"
+        case .patchLeaveChatRoom(let roomId):
+            return "rooms/\(roomId)/leave"
+        case .patchExitChatRoom(let roomId):
+            return "rooms/\(roomId)/exit"
+        case .getChatRoomIds:
+            return "rooms/ids"
         }
     }
     
     var method: Moya.Method {
         switch self {
-        case .patchEnterChatRoom:
+        case .patchEnterChatRoom, .patchLeaveChatRoom, .patchExitChatRoom:
             return .patch
         case .postCreateChatRoom:
             return .post
@@ -77,8 +90,20 @@ extension ChatAPI: BaseTargetType {
     
     var task: Moya.Task {
         switch self {
+        case .getChatInfo(_, let roomId):
+            if let roomId {
+                return .requestParameters(parameters: ["roomId" : roomId], encoding: URLEncoding.queryString)
+            } else {
+                return .requestPlain
+            }
         case .postCreateChatRoom(let requestBody):
             return .requestJSONEncodable(requestBody)
+        case .getChatRooms(let deviceToken):
+            if let deviceToken {
+                return .requestParameters(parameters: ["deviceToken" : deviceToken], encoding: URLEncoding.queryString)
+            } else {
+                return .requestPlain
+            }
         default:
             return .requestPlain
         }
--- Napzak-iOS/Napzak-iOS/Network/Chat/ChatService.swift
+++ Napzak-iOS/Napzak-iOS/Network/Chat/ChatService.swift
@@ -9,17 +9,22 @@ import Foundation
 import Moya
 
 protocol ChatServiceProtocol {
-    func getChatInfo(productId: Int) async -> Result<ChatDetailResponseDTO, NetworkError>
+    func getChatInfo(productId: Int, roomId: Int?) async -> Result<ChatDetailResponseDTO, NetworkError>
     func postCreateChatRoom(requestBody: ChatRoomCreateRequestDTO) async -> Result<ChatRoomCreateResponseDTO, NetworkError>
     func patchEnterChatRoom(roomId: Int) async -> Result<ChatRoomEnterResponseDTO, NetworkError>
+    func getChatMessages(roomId: Int) async -> Result<ChatMessageResponseDTO, NetworkError>
+    func getChatRooms(deviceToken: String?) async -> Result<ChatRoomResponseDTO, NetworkError>
+    func patchLeaveChatRoom(roomId: Int) async -> Result<Void, NetworkError>
+    func patchExitChatRoom(roomId: Int) async -> Result<Void, NetworkError>
+    func getChatRoomIds() async -> Result<ChatRoomIdListResponseDTO, NetworkError>
 }
 
 final class ChatService: BaseService, ChatServiceProtocol {
     
     private let provider = MoyaProvider<ChatAPI>.init(plugins: [MoyaPlugin()])
         
-    func getChatInfo(productId: Int) async -> Result<ChatDetailResponseDTO, NetworkError> {
-        return await requestDecodable(provider, .getChatInfo(productId: productId))
+    func getChatInfo(productId: Int, roomId: Int?) async -> Result<ChatDetailResponseDTO, NetworkError> {
+        return await requestDecodable(provider, .getChatInfo(productId: productId, roomId: roomId))
     }
     
     func postCreateChatRoom(requestBody: ChatRoomCreateRequestDTO) async -> Result<ChatRoomCreateResponseDTO, NetworkError> {
@@ -28,5 +33,25 @@ final class ChatService: BaseService, ChatServiceProtocol {
     
     func patchEnterChatRoom(roomId: Int) async -> Result<ChatRoomEnterResponseDTO, NetworkError> {
         return await requestDecodable(provider, .patchEnterChatRoom(roomId: roomId))
+    }
+    
+    func getChatMessages(roomId: Int) async -> Result<ChatMessageResponseDTO, NetworkError> {
+        return await requestDecodable(provider, .getChatMessages(roomId: roomId))
+    }
+    
+    func getChatRooms(deviceToken: String? = nil) async -> Result<ChatRoomResponseDTO, NetworkError> {
+        return await requestDecodable(provider, .getChatRooms(deviceToken: deviceToken))
+    }
+    
+    func patchLeaveChatRoom(roomId: Int) async -> Result<Void, NetworkError> {
+        return await requestVoid(provider, .patchLeaveChatRoom(roomId: roomId))
+    }
+    
+    func patchExitChatRoom(roomId: Int) async -> Result<Void, NetworkError> {
+        return await requestVoid(provider, .patchExitChatRoom(roomId: roomId))
+    }
+    
+    func getChatRoomIds() async -> Result<ChatRoomIdListResponseDTO, NetworkError> {
+        return await requestDecodable(provider, .getChatRoomIds)
     }
 }
