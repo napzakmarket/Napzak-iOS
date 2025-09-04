b4fcd5fd7582cc424f9e0a0b41c52d44b71e961e
JoHyerin <gpfls468@gmail.com>
2025-09-05 04:25:22 +0900

    [Feat] #179 - 마켓 관련 믹스패널 연결

--- Napzak-iOS/Napzak-iOS/Presentation/Market/MarketView.swift
+++ Napzak-iOS/Napzak-iOS/Presentation/Market/MarketView.swift
@@ -28,6 +28,7 @@ struct MarketView: View {
     
     //MARK: - Properties
 
+    private let mixpanelManager = MixpanelManager.shared
     private let productCellWidth = (UIScreen.main.bounds.width - 76) / 2
     private let columns = [GridItem(.flexible(), spacing: 20), GridItem(.flexible())]
     
@@ -130,6 +131,7 @@ struct MarketView: View {
         .animation(.easeInOut(duration: 0.3), value: isGenreSelectModalPresented)
         .animation(.easeInOut(duration: 0.3), value: isSortModalPresented)
         .onAppear {
+            mixpanelManager.trackEvent(event: "Viewed MyPage")
             Task {
                 await viewModel.fetchStoreDetail()
             }
