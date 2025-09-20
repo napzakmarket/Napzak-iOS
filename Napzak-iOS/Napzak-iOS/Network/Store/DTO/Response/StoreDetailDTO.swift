2e1be2d6d3500b5b1d118fa31afc759795f77d83
JoHyerin <gpfls468@gmail.com>
2025-09-21 01:35:18 +0900

    [Fix] #187 - 마켓 정보 조회 DTO 수정

--- Napzak-iOS/Napzak-iOS/Network/Store/DTO/Response/StoreDetailDTO.swift
+++ Napzak-iOS/Napzak-iOS/Network/Store/DTO/Response/StoreDetailDTO.swift
@@ -12,6 +12,7 @@ struct StoreDetailDTO: Decodable {
     let storePhoto: String?
     let storeCover: String?
     let isStoreOwner: Bool
+    let isStoreBlocked: Bool
     let genrePreferences: [GenreDTO]
 }
 
