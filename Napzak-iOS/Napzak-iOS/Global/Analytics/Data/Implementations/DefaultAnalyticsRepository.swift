ff98a4e28b209c6299b5dcb7f1d3b2040c4b4f31
OneTen19 <khy000426@naver.com>
2026-04-21 23:29:46 +0900

    [Feat] #196 - AnalyticsRepository Dependency

--- Napzak-iOS/Napzak-iOS/Global/Analytics/Data/Implementations/DefaultAnalyticsRepository.swift
+++ Napzak-iOS/Napzak-iOS/Global/Analytics/Data/Implementations/DefaultAnalyticsRepository.swift
@@ -28,3 +28,18 @@ public final class DefaultAnalyticsRepository: AnalyticsRepository {
         await service.sendEvent(name: eventName, parameters: parsedParameters)
     }
 }
+
+
+// MARK: - Dependency
+
+extension DefaultAnalyticsRepository {
+    /// 싱글톤(shared)이라는 이름 대신, 실서비스용 객체라는 의미의 live 사용
+    /// 지연 초기화 방식으로 실제 사용 시점부터 메모리에 올라가고 하나의 객체를 재사용
+    /// 기존 Napzak_iOSApp의 init에서 선언하는 방식에 비해 앱 초기 구동 속도에 이점이 있음
+    /// DIContainer등을 활용하는 방향으로 개선하면 이상적이겠으나, 규모가 너무 커서 이 방안으로 타협했음 ㅜ
+    public static let live: AnalyticsRepository = {
+        let service = MixpanelAnalyticsService()
+        service.initialize()
+        return DefaultAnalyticsRepository(service: service)
+    }()
+}
